6c103a09ca5417c5ec27b5e6b8aaabeefbda9c6a
Yash Kumar Raut <yashfreakin1@gmail.com>
2026-02-28 13:03:28 +0530

    Time: 1 ms (99.91%) | Memory: 46.6 MB (96.35%) - LeetSync

--- 136-single-number/single-number.java
+++ 136-single-number/single-number.java
@@ -1,22 +1,33 @@
+// class Solution {
+//     public int singleNumber(int[] arr) {
+//         int n = arr.length;
+        
+//         for (int i = 0; i < n; i++) {
+//             boolean foundDuplicate = false;
+            
+//             for (int j = 0; j < n; j++) {
+//                 if (i != j && arr[i] == arr[j]) {
+//                     foundDuplicate = true;
+//                     break;
+//                 }
+//             }
+            
+//             if (!foundDuplicate) {
+//                 return arr[i];
+//             }
+//         }
+        
+//         return -1;
+//     }
+// }
+
 class Solution {
     public int singleNumber(int[] arr) {
         int n = arr.length;
-        
-        for (int i = 0; i < n; i++) {
-            boolean foundDuplicate = false;
-            
-            for (int j = 0; j < n; j++) {
-                if (i != j && arr[i] == arr[j]) {
-                    foundDuplicate = true;
-                    break;
-                }
-            }
-            
-            if (!foundDuplicate) {
-                return arr[i];
-            }
+        int sum = 0;
+        for(int i = 0; i<n; i++){
+            sum=sum^arr[i];
         }
-        
-        return -1;
+        return sum;
     }
-}
+}    
