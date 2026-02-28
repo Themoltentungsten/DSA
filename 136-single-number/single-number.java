fddd3206eab7ccbfd52aa41760447c5e75c86735
Yash Kumar Raut <yashfreakin1@gmail.com>
2026-02-28 12:57:54 +0530

    Time: 213 ms (8.46%) | Memory: 47.1 MB (45.15%) - LeetSync

---
+++ 136-single-number/single-number.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int singleNumber(int[] arr) {
+        int n = arr.length;
+        
+        for (int i = 0; i < n; i++) {
+            boolean foundDuplicate = false;
+            
+            for (int j = 0; j < n; j++) {
+                if (i != j && arr[i] == arr[j]) {
+                    foundDuplicate = true;
+                    break;
+                }
+            }
+            
+            if (!foundDuplicate) {
+                return arr[i];
+            }
+        }
+        
+        return -1;
+    }
+}
